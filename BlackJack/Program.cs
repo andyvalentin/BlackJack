3a46f8b057f2ab00377a7f92c019bdd555646d06
Andy <andyvalentin01@gmail.com>
2016-01-17 11:03:54 -0600

    Initial Commit - Card class created

---
+++ BlackJack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Card
+    {
+        public int NumValue { get; set; }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
